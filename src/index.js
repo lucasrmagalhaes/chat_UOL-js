0735dbb654d51fa160c2707e941504b8639a20d7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-03-01 15:07:42 -0300

    iniciando o projeto

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'reset-css';
+import './styles/index.css';
+
+console.log('App running');
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      favicon: './src/assets/ie-icon.png',
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
